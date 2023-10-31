7253aa9b5285e91f1acac44c063b73d55c1024e7
Wahid <macbookair@MacBooks-MacBook-Air.local>
2023-10-31 10:26:51 +0600

    added header and changed home title

--- src/App.js
+++ src/App.js
@@ -1,34 +1,32 @@
-import { createContext, useState } from 'react';
-import { Toaster } from 'react-hot-toast';
-import { Route, Routes } from 'react-router-dom';
-import About from './Pages/About/About';
-import Blogs from './Pages/Blogs/Blogs';
-import Contact from './Pages/Contact/Contact';
-import Home from './Pages/Home/Home';
-import ProjectDetail from './Pages/ProjectDetail/ProjectDetail';
-import Navbar from './Pages/Shared/Navbar';
+import { createContext, useState } from "react";
+import { Toaster } from "react-hot-toast";
+import { Route, Routes } from "react-router-dom";
+import About from "./Pages/About/About";
+import Blogs from "./Pages/Blogs/Blogs";
+import Contact from "./Pages/Contact/Contact";
+import Home from "./Pages/Home/Home";
+import ProjectDetail from "./Pages/ProjectDetail/ProjectDetail";
+import Navbar from "./Pages/Shared/Navbar";
 
 export const WorkContext = createContext();
 
 function App() {
   const [works, setWorks] = useState([]);
   return (
-    <WorkContext.Provider value={{works, setWorks}}>
+    <WorkContext.Provider value={{ works, setWorks }}>
       <div
         style={{ fontFamily: "'Libre Franklin', sans-serif" }}
-        className='bg-base-100 text-white h-full'>
+        className="bg-base-100 text-white h-full"
+      >
         <Navbar />
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/about' element={<About />} />
-          <Route path='/contact' element={<Contact />} />
-          <Route path='/blogs' element={<Blogs />} />
-          <Route path='/project/:id' element={<ProjectDetail />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          {/* <Route path='/blogs' element={<Blogs />} /> */}
+          <Route path="/project/:id" element={<ProjectDetail />} />
         </Routes>
-        <Toaster
-          position="top-center"
-          reverseOrder={false}
-        />
+        <Toaster position="top-center" reverseOrder={false} />
       </div>
     </WorkContext.Provider>
   );
